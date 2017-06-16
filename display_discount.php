aed11c25cdae8883891c7ac8bbd7357a3be62aa2
monika pattanayak <mp657@njit.edu>
2017-06-15 23:42:17 -0400

    added code to claculate discount

--- display_discount.php
+++ display_discount.php
@@ -3,6 +3,10 @@
 	$product_description = $_POST['product_description'];
 	$list_price = $_POST['list_price'];
 	$discount_percent = $_POST['discount_percent'];
+	
+	// calculate the discount
+	$discount = $list_price * $discount_percent * .01;
+	$discount_price = $list_price - $discount;
 ?>	
 
 <!DOCTYPE html>
